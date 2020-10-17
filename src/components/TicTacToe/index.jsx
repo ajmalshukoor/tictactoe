6a9f618c61506e5c3b4fc89c573422556b5236f4
ajmalshukoor <ajmalshukoor11@gmail.com>
2020-10-17 12:37:56 -0400

    init

---
+++ src/components/TicTacToe/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Header from '../shared/Header';
+
+const TicTacToe = () => {
+  return (  
+    <>    
+    <Header title="Let's Do This Thang"/>
+    </>
+  );
+}
+ 
+export default TicTacToe;
